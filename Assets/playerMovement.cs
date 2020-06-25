3f3a3fc30c49f84fce73058328e2c6c66e7e14ff
Gonzako <al341522@uji.es>
2020-06-25 13:09:51 +0200

    Fixed build problems, added layer colision matrix, WIP player prefab

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class playerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 2;
+
+    Vector2 inputValue;
+    Vector2 desiredVelocity;
+
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnMovement(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        inputValue = context.ReadValue<Vector2>();
+        Debug.Log(inputValue);
+    }
+
+    private void FixedUpdate()
+    {
+        desiredVelocity = inputValue * speed;
+        rb.velocity = desiredVelocity;
+    }
+}
