e7b62e3e9f7a7af85aab639e4b393fc3a09028f7
Gonzako <al341522@uji.es>
2020-07-03 21:47:15 +0200

    Added in wall of cancer - need to implement death and chase logic

---
+++ Assets/tweenToPositionOnEnable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class tweenToPositionOnEnable : MonoBehaviour
+{
+
+    public Vector3 positionToTweenTowards;
+    public float timeToGetThere = 5f;
+
+
+    private void OnEnable()
+    {
+        transform.DOMove(positionToTweenTowards, timeToGetThere).SetEase(Ease.InOutQuad);
+    }
+
+
+}
